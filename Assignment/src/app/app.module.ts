b0889d81de169cc897612db40d21858e98ca818d
JayShenkar07 <jayshenkar27apr@gmail.com>
2024-05-07 01:07:49 +0530

    UI-Components Creation

--- Assignment/src/app/app.module.ts
+++ Assignment/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BooksComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
