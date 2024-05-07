94b3f261e87e190f9ea6840204c08d604ee8c4a0
JayShenkar07 <jayshenkar27apr@gmail.com>
2024-05-07 14:48:32 +0530

    Feat: Implemented fetching data from API in Details component with service

--- Assignment/src/app/details/details.component.ts
+++ Assignment/src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FetchService } from '../fetch.service';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+
+  constructor(private route: ActivatedRoute,private http: HttpClient, private fetch: FetchService){}
+
+  Bookid:any=""  //globally to store the qurey param
+  bookDetails: any;
+  loading: boolean = true;
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(val => {
+      this.Bookid=val['id'];
+      console.log('Received query parameter:',this.Bookid);
+
+    this.fetch.getBookDetails(this.Bookid).subscribe(
+      (details: any) => {
+        // console.log('Book details:', details);
+        this.bookDetails = details;
+        this.loading=false;
+      },
+
+      error => {
+        console.error('Ops! Error fetching book details:', error);
+      }
+    );
+  });
+}
 
 }
